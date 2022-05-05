10d74b5eebcad1c2e4b7f1c12e5e2038659e196c
Shrey Pandey <shreypandey27@gmail.com>
2022-05-05 16:09:22 +0530

    pin a note added

--- src/frontend/pages/homepage/Notes.js
+++ src/frontend/pages/homepage/Notes.js
@@ -32,6 +32,10 @@ export function Notes({
     }
   };
 
+  const handlePinned = (note) => {
+    handleNewNote({ ...note, pinned: !note.pinned ? true : !note.pinned });
+  };
+
   const handleRestoreFromTrash = (note) => {
     if (notes.some((item) => item.trash)) {
       handleNewNote({ ...note, trash: false });
@@ -40,68 +44,147 @@ export function Notes({
 
   return (
     <Fragment>
-      <div className='notes'>
-        {!notes.length ? (
-          <Empty />
-        ) : (
-          notes?.map((note, index) => {
-            return (
-              <div
-                className='singlenote'
-                key={note.id}
-                style={{ backgroundColor: note.color }}
-              >
-                <h1>{note.title}</h1>
-                <p>{note.description}</p>
-                {note.label && <h2>{note.label}</h2>}
-                <section className='note__section'>
-                  {pathname === HOMEPAGE && (
-                    <button className='btn--icon'>
-                      <i className='fa-solid fa-thumbtack'></i>
-                    </button>
-                  )}
-                  {(pathname === HOMEPAGE ||
-                    pathname === ARCHIVE ||
-                    pathname === LABEL) && (
-                    <button
-                      className='btn--icon'
-                      onClick={handleArchive.bind(this, note)}
-                    >
-                      <i className='fa-solid fa-box-archive'></i>
-                    </button>
-                  )}
-                  {(pathname === HOMEPAGE ||
-                    pathname === TRASH ||
-                    pathname === LABEL) && (
-                    <button
-                      className='btn--icon'
-                      onClick={handleDelete.bind(this, note)}
-                    >
-                      <i className='fa-solid fa-trash'></i>
-                    </button>
-                  )}
-                  {pathname === TRASH && (
-                    <button
-                      className='btn--icon'
-                      onClick={handleRestoreFromTrash.bind(this, note)}
-                    >
-                      <i className='fa-solid fa-arrow-rotate-left'></i>
-                    </button>
-                  )}
-                  {(pathname === HOMEPAGE || pathname === LABEL) && (
-                    <button
-                      className='btn--icon btn--rt'
-                      onClick={handleEdit.bind(this, note)}
-                    >
-                      <i className='fa-solid fa-pencil'></i>
-                    </button>
-                  )}
-                </section>
-              </div>
-            );
-          })
-        )}
-      </div>
+      {!notes.length ? (
+        <Empty />
+      ) : (
+        <>
+          {notes?.some((item) => item.pinned) && (
+            <h1 className='pinned'>Pinned Notes</h1>
+          )}
+          {/* Pinned notes */}
+          <div className='notes'>
+            {notes
+              ?.filter((item) => item.pinned)
+              ?.map((note, index) => {
+                return (
+                  <div
+                    className='singlenote'
+                    key={note.id}
+                    style={{ backgroundColor: note.color }}
+                  >
+                    <h1>{note.title}</h1>
+                    <p>{note.description}</p>
+                    {note.label && <h2>{note.label}</h2>}
+                    <section className='note__section'>
+                      {pathname === HOMEPAGE && (
+                        <button
+                          className='btn--icon'
+                          onClick={handlePinned.bind(this, note)}
+                        >
+                          <i
+                            className={`fa-solid fa-thumbtack ${
+                              note.pinned && 'icon__pinned'
+                            }`}
+                          ></i>
+                        </button>
+                      )}
+                      {(pathname === HOMEPAGE ||
+                        pathname === ARCHIVE ||
+                        pathname === LABEL) && (
+                        <button
+                          className='btn--icon'
+                          onClick={handleArchive.bind(this, note)}
+                        >
+                          <i className='fa-solid fa-box-archive'></i>
+                        </button>
+                      )}
+                      {(pathname === HOMEPAGE ||
+                        pathname === TRASH ||
+                        pathname === LABEL) && (
+                        <button
+                          className='btn--icon'
+                          onClick={handleDelete.bind(this, note)}
+                        >
+                          <i className='fa-solid fa-trash'></i>
+                        </button>
+                      )}
+                      {pathname === TRASH && (
+                        <button
+                          className='btn--icon'
+                          onClick={handleRestoreFromTrash.bind(this, note)}
+                        >
+                          <i className='fa-solid fa-arrow-rotate-left'></i>
+                        </button>
+                      )}
+                      {(pathname === HOMEPAGE || pathname === LABEL) && (
+                        <button
+                          className='btn--icon btn--rt'
+                          onClick={handleEdit.bind(this, note)}
+                        >
+                          <i className='fa-solid fa-pencil'></i>
+                        </button>
+                      )}
+                    </section>
+                  </div>
+                );
+              })}
+          </div>
+          {/* Unpinned notes */}
+          <div className='notes'>
+            {notes
+              ?.filter((item) => !item.pinned)
+              ?.map((note, index) => {
+                return (
+                  <div
+                    className='singlenote'
+                    key={note.id}
+                    style={{ backgroundColor: note.color }}
+                  >
+                    <h1>{note.title}</h1>
+                    <p>{note.description}</p>
+                    {note.label && <h2>{note.label}</h2>}
+                    <section className='note__section'>
+                      {pathname === HOMEPAGE && (
+                        <button
+                          className='btn--icon'
+                          onClick={handlePinned.bind(this, note)}
+                        >
+                          <i className='fa-solid fa-thumbtack'></i>
+                        </button>
+                      )}
+                      {(pathname === HOMEPAGE ||
+                        pathname === ARCHIVE ||
+                        pathname === LABEL) && (
+                        <button
+                          className='btn--icon'
+                          onClick={handleArchive.bind(this, note)}
+                        >
+                          <i className='fa-solid fa-box-archive'></i>
+                        </button>
+                      )}
+                      {(pathname === HOMEPAGE ||
+                        pathname === TRASH ||
+                        pathname === LABEL) && (
+                        <button
+                          className='btn--icon'
+                          onClick={handleDelete.bind(this, note)}
+                        >
+                          <i className='fa-solid fa-trash'></i>
+                        </button>
+                      )}
+                      {pathname === TRASH && (
+                        <button
+                          className='btn--icon'
+                          onClick={handleRestoreFromTrash.bind(this, note)}
+                        >
+                          <i className='fa-solid fa-arrow-rotate-left'></i>
+                        </button>
+                      )}
+                      {(pathname === HOMEPAGE || pathname === LABEL) && (
+                        <button
+                          className='btn--icon btn--rt'
+                          onClick={handleEdit.bind(this, note)}
+                        >
+                          <i className='fa-solid fa-pencil'></i>
+                        </button>
+                      )}
+                    </section>
+                  </div>
+                );
+              })}
+          </div>
+        </>
+      )}
     </Fragment>
   );
 }
